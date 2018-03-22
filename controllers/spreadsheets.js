237c61f7860853e228c348d0296813efe69aa7d1
Mikael Mello <mikaelmmello@gmail.com>
2018-03-22 10:01:34 -0300

    Adiciona novos scripts ao package e muda novamente a planilha.
    Muda a planilha para uma customizada que gera um gráfico legível
    Adiciona scripts start e lint para iniciar o programa e verificar o style.
    Adiciona hook de precommit para rodar o lint e tests antes de commits.

--- controllers/spreadsheets.js
+++ controllers/spreadsheets.js
@@ -44,7 +44,7 @@ let listCollectives = (auth) => {
 		const sheets = google.sheets("v4");
 		sheets.spreadsheets.values.get({
 			auth: auth,
-			spreadsheetId: "1NkTrhXFGQ68VIF9PVxunXaEU_PVFJhgDx-qCsf1iH5o",
+			spreadsheetId: "1W4kmkNTZrDUHyZx_siToEr9EDZvTGEwDTp05I18Ach4",
 			range: "A:S"
 		}, (err, res) => {
 			if (err) {
@@ -73,7 +73,7 @@ let listCollectives = (auth) => {
  * chartJSNode fails to do so.
  */
 let generateCharts = (collectives) => {
-  console.log(JSON.stringify(collectives));
+	console.log(JSON.stringify(collectives));
 	const chartNode = new ChartjsNode(600, 600);
 	/* In sequence: Tweets, Seguindo, Seguidores, Curtidas */
 	const data = [collectives[2][8], collectives[2][9], collectives[2][10], collectives[2][11]];

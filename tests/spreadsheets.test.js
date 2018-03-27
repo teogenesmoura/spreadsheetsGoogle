af424d11245644d166ccdcc9f89740efb0355b65
Mikael Mello <mikaelmmello@gmail.com>
2018-03-27 13:12:40 -0300

    Adiciona novas regras de linting e refatora testes
    Lint agora tem ambiente lint e extende airbnb-base
    Apenas mudanças de lint em spreadsheets.js e index.js
    Teste de spreadsheets agora compara com uma imagem pré-existente
    Imagens na pasta controllers agora são ignoradas

--- tests/spreadsheets.test.js
+++ tests/spreadsheets.test.js
@@ -1,13 +1,26 @@
-"use strict";
 const spreadsheets = require("../controllers/spreadsheets");
 const fs = require("fs");
 /**
- * Test case for the 'generateCharts' method. It loads a mock collectives.json file (In the time of writing the file reflects
- * the current state of the spreadsheet) and tests whether a chart is saved to the disk or not.
+ * Test case for the 'generateCharts' method. It loads a mock collectives.json file (In the time of
+ * writing the file reflects the current state of the spreadsheet) and tests whether a chart is
+ * saved to the disk or not.
  */
-it("frentePopularInstagram chart should exist on disk", () => {
-	expect.assertions(1);
-	var collectives = JSON.parse(fs.readFileSync("./collectives.json"));
-	return spreadsheets.generateCharts(collectives)
-		.then(expect(fs.readFileSync("./controllers/frentePopularInstagram.png")).toBeInstanceOf(Buffer));
+describe("generateCharts method", () => {
+	it("frentePopularInstagram chart should be created", (done) => {
+		// loads json file containing the values of a spreadsheet
+		const collectives = JSON.parse(fs.readFileSync("./tests/spreadsheets-collectives.json"));
+
+		// generates the chart from the existent file
+		spreadsheets.generateCharts(collectives)
+			.then(() => {
+				// loads the created image
+				const newImage = fs.readFileSync("./controllers/frentePopularInstagram.png");
+				// loads the pre-existing correct image
+				const oldImage = fs.readFileSync("./tests/spreadsheets-instagram-chart.png");
+				expect(newImage).toEqual(oldImage); // images have to be the same
+				expect(newImage).toBeInstanceOf(Buffer);
+				done();
+			})
+			.catch(done);
+	});
 });

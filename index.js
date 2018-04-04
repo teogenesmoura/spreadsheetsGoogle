0e80ee47a80b29f098d59ec9676274824a8ab196
Mikael Mello <mikaelmmello@gmail.com>
2018-04-04 13:44:36 -0300

    Adiciona mongoose e configurações usando dot-env
    Configura conexão inicial com mongoose usando bluebird de Promise
    
    App configurado a partir do arquivo .env (dot-env)

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const mongoose = require("mongoose");
 const app = require("./config/express");
 const logger = require("./config/logger");
+const config = require("./config/config");
+
+Promise = require("bluebird"); // eslint-disable-line no-global-assign
+
+// plugin bluebird promise in mongoose
+mongoose.Promise = Promise;
+
+// connect to mongo db
+const mongoUri = config.mongo.host;
+mongoose.connect(mongoUri, { keepAlive: 1 });
+mongoose.connection.on("error", () => {
+	throw new Error(`unable to connect to database: ${mongoUri}`);
+});
 
 app.listen(3000, () => {
 	logger.info(`[SERVER] Listening on port ${3000}`);

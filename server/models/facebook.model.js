35ca48504e5361a32b29e2017312a0f0107460fb
rodrigofegui <rodrigofegui@gmail.com>
2018-04-05 09:36:01 -0300

    Add: modelo inicial dos dados manipulados do Facebook; Contém observações, potencialmente relevantes, anotadas; Add: gitIgnore -> credentials.json, no seu novo local

---
+++ server/models/facebook.model.js
@@ -0,0 +1,48 @@
+// Módulo do MongoDB, banco de dados NoSQL
+const mongoose = require("mongoose");
+
+const nomeModelo = "facebookConta";
+/**
+ * Planilha que irá povoar o BD:
+ * https://docs.google.com/spreadsheets/d/1yesZHlR3Mo0qpuH7VTFB8_zyl6p_H-b1khh-wlB3O_Q/edit#gid=0
+ * Identificação primária das colunas para os dados do Facebook:
+ * 		Nome			= Linha: 3[2], Coluna: A[0],
+ *		Conta			= Linha: 2[1], Coluna: D[3],
+ *		Curtidas		= Linha: 2[1], Coluna: E[4],
+ *		Seguidos		= Linha: 2[1], Coluna: F[5],
+ *		Data de Coleta	= Linha: 2[1], Coluna: G[6],
+ *		Nome das Planilhas: "Grupos/<mes> <ano>",
+ *			<mes> => nome em 3 letras,
+ *			<ano> => 4 digitos
+ *		Classe: 03-13 => Frente/Coletivos,
+ *				16-53 => Organizações da Sociedade Civil,
+ *				56-77 => Pré-Candidatura à Presidência
+ * Verificar o significado dos "Não usar --";
+ * Vale a pena já desenvolver os métodos de pesquisa básicos?
+ * Como testar as interfaces com o MongoDB?
+*/
+
+const facebookContaEsquema = new mongoose.Schema({
+	nome: {	// name
+		type: String,
+	},
+	classe: { // class, cluster
+		type: String,
+	},
+	link: { // acccount, fanPage, profile
+		type: String,
+		trim: true,
+	},
+	curtidas: { // likes
+		type: Number,
+	},
+	seguidores: { // followers
+		type: Number,
+	},
+	mesConsulta: { // consultation, query
+		type: Date,
+		Default: Date.now,
+	},
+});
+
+module.exports = mongoose.model(nomeModelo, facebookContaEsquema);

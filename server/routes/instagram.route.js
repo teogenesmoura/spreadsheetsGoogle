544779a142541a0d8ee202c573ef7f6de623c7f3
Samuel Couto <samuelcoouto@hotmail.com>
2018-04-12 13:41:00 -0300

    Adicionada rota que liga à impressão dos dados do DB
    Co-authored-by: Pedro Ivo <pedroivomachado@hotmail.com>

--- server/routes/instagram.route.js
+++ server/routes/instagram.route.js
@@ -1,11 +1,16 @@
 const express = require("express");
-/* const instagramCtrl = require("../controllers/instagram.controller"); */
+
+const instagramControl = require("../controllers/instagram.controller");
 
 const router = express.Router(); // eslint-disable-line new-cap
+
 
 router.get("/", (req, res) => {
 	res.write("Hello insta");
 	res.end();
 });
+
+router.route("/all")
+	.get(instagramControl.listAccounts);
 
 module.exports = router;

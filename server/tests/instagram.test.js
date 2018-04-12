32f2c88c4eaf8408f3e5cc78e05c86fd4818f629
Samuel Couto <samuelcoouto@hotmail.com>
2018-04-12 13:46:09 -0300

    Testes implementados, comentado por conta de bug
    Co-authored-by: Pedro Ivo <pedroivomachado@hotmail.com>

--- server/tests/instagram.test.js
+++ server/tests/instagram.test.js
@@ -1,13 +1,25 @@
 const request = require("supertest");
 const httpStatus = require("http-status");
 const app = require("../../index");
+const instagramAccountModel = require("../models/instagram.model");
+const instagramStub = require("./instagram-stub.json").instagram;
 
 /**
  * Tests if instagram endpoint can be reached
  */
 
+beforeAll(async () => {
+	await instagramAccountModel.collection.insert(instagramStub);
+});
+
+afterAll(async () => {
+	await instagramAccountModel.collection.drop();
+});
+
 describe("GET /instagram", () => {
-	it("test the index of /instagram", (done) => {
+//	let nameTest;
+
+	it("should reach /instagram", (done) => {
 		request(app)
 			.get("/instagram")
 			.expect(httpStatus.OK)
@@ -16,4 +28,25 @@ describe("GET /instagram", () => {
 			})
 			.catch(done);
 	});
+/*	it("should access /instagram/all", async (done) => {
+		const res = await request(app).get("/instagram/all").expect(httpStatus.OK);
+
+		expect(res.body).toHaveProperty("error");
+		expect(res.body.error).toBe(false);
+
+		expect(res.body).toHaveProperty("usernames");
+		expect(res.body.usernames).toBeInstanceOf(Array);
+//		expect(res.body.username.length).toEqual(instagramStub.length);
+		nameTest = res.body.usernames[0];
+
+		done();
+	});
+	it("should return data from /instagram/all", async (done) => {
+		expect(nameTest).toBeDefined();
+
+		const res = await request(app).get("/instagram/all").expect(httpStatus.OK);
+
+		done();
+	});
+*/
 });

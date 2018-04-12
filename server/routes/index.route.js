a052a32224328fcd4dacc7ac0b27cad4d34ca8b2
Samuel Couto <samuelcoouto@hotmail.com>
2018-04-11 21:58:48 -0300

    Route para /instagram adicionado

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const spreadsheetsRoute = require("./spreadsheets.route");
+const instagramRoute = require("./instagram.route");
 
 const router = express.Router();
 
 // mount spreadsheets routes at /spreadsheets
 router.use("/spreadsheets", spreadsheetsRoute);
+
+router.use("/instagram", instagramRoute);
 
 module.exports = router;

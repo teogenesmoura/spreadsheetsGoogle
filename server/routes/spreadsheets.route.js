adee3f05c0732e1a5fcab6d6bce4b4575d05bf75
Rodrigo F. Guimarães <rodrigofegui@gmail.com>
2018-06-02 17:26:29 -0300

    Finalização da página inicial
    Add:
    - Opções oferecidas pelo portal.
    
    Co-Authored-By: Pedro Ivo Machado <pedroivomachado@hotmail.com>

--- server/routes/spreadsheets.route.js
+++ server/routes/spreadsheets.route.js
@@ -6,8 +6,4 @@ const router = express.Router(); // eslint-disable-line new-cap
 router.route("/")
 	.get(spreadsheetsCtrl.showLinkMap);
 
-router.get("/qualquer", (req, res) => {
-	res.render("qualquer");
-});
-
 module.exports = router;

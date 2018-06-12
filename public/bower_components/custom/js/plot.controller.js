63c7bc65c5fbc9cd8aba644a3410ae318b75cd88
rodrigofegui <rodrigofegui@gmail.com>
2018-06-12 11:04:39 -0300

    Comportamento da Lista de atores.
    Add:
    - Muda a lista conforme a categoria escolhida.
    
    Co-Authored-By: Rodrigo Guimarães <rodrigofegui@gmail.com>

--- public/bower_components/custom/js/plot.controller.js
+++ public/bower_components/custom/js/plot.controller.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(() => {
 	const query = {
 		Selecione: ["Selecione"],
@@ -19,12 +20,182 @@ $(document).ready(() => {
 
 		
 	}).change();
-	// */
 });
+// */
+
+$(document).ready(() => {
+	$("#digitalMedia").on("change", () => {
+		let media = $("#digitalMedia").val();
+
+        switch(media){
+        	case "facebook": facebookQueries(); break;
+            case "instagram": instagramQueries(); break;
+            case "twitter": twitterQueries(); break;
+            case "youtube": youtubeQueries(); break;
+            default: clear("queries");
+        }
+    });
+});
+
+$(document).ready(() => {
+	$("#queries").on("change", () => {
+		let media = $("#queries").val();
+		
+        $("body").append("<br>Leu " + media + "<br>");
+    });
+});
+
+let facebookQueries = () => {
+	clear("queries");
+	$("#queries").append("<option value='likes'>Curtidas</option>");
+    $("#queries").append("<option value='followers'>Seguidores</option>");
+}
+
+let instagramQueries = () => {
+	clear("queries");
+	$("#queries").append("<option value='followers'>Seguidores</option>"); 
+    $("#queries").append("<option value='following'>Seguindo</option>");
+    $("#queries").append("<option value='num_of_posts'>Postagens</option>");
+}
+
+let twitterQueries = () => {
+	clear("queries");
+	$("#queries").append("<option value='tweets'>Tweets</option>");
+    $("#queries").append("<option value='followers'>Seguidores</option>");
+    $("#queries").append("<option value='following'>Seguindo</option>");
+    $("#queries").append("<option value='likes'>Curtidas</option>");
+    $("#queries").append("<option value='moments'>Momentos</option>");
+}
+
+let youtubeQueries = () => {
+	clear("queries");
+	$("#queries").append("<option value='videos'>Vídeos</option>");
+    $("#queries").append("<option value='views'>Visualizações</option>");
+    $("#queries").append("<option value='subscribers'>Inscritos</option>");
+}
+
+let clear = (field) => {
+	switch(field){
+		case "queries": clearQueries(); break;
+		case "categories": clearCategories(); break;
+	}
+}
+
+let clearQueries = () => {
+	$("#queries").empty();
+    $("#queries").prepend("<option value=''>Escolha...</option>");
+}
+
+let clearCategories = () => {
+	$("#categories").empty();
+}
+
+$(document).ready(() => {
+	$("#categories").on("change", () => {
+		let category = $("#categories").val();
+
+        switch(category){
+        	case "F/C": getActorsFC(); break;
+            case "OSC": getActorsOSC(); break;
+            case "CP": getActorsCP(); break;
+            default: clear("categories");
+        }
+    });
+});
+
+let getActorsFC = () => {
+	$("#checkbox").empty();
+
+	let actors = [
+		{
+			name: "Ator 0",
+			id: "id_0",
+		},
+		{
+			name: "Ator 1",
+			id: "id_1",
+		},
+		{
+			name: "Ator 2",
+			id: "id_2",
+		},
+	];
+
+	// get(atores)
+
+	actors.forEach((actor) => {
+		let check = "<input class='form-check-input' type='checkbox' id='" + actor.id + "'/>";
+		$("#checkbox").append(check);
+		check = "<label class='form-check-label' for='" + actor.id + "'>" + actor.name + "</label><br>";
+		$("#checkbox").append(check);
+
+
+	});
+};
+
+let getActorsOSC = () => {
+	$("#checkbox").empty();
+
+	let actors = [
+		{
+			name: "Ator 3",
+			id: "id_3",
+		},
+		{
+			name: "Ator 4",
+			id: "id_4",
+		},
+		{
+			name: "Ator 5",
+			id: "id_5",
+		},
+	];
+
+	// get(atores)
+
+	actors.forEach((actor) => {
+		let check = "<input class='form-check-input' type='checkbox' id='" + actor.id + "'/>";
+		$("#checkbox").append(check);
+		check = "<label class='form-check-label' for='" + actor.id + "'>" + actor.name + "</label><br>";
+		$("#checkbox").append(check);
+
+
+	});
+};
+let getActorsCP = () => {
+	$("#checkbox").empty();
+
+	let actors = [
+		{
+			name: "Ator 6",
+			id: "id_6",
+		},
+		{
+			name: "Ator 7",
+			id: "id_7",
+		},
+		{
+			name: "Ator 8",
+			id: "id_8",
+		},
+	];
+
+	// get(atores)
+
+	actors.forEach((actor) => {
+		let check = "<input class='form-check-input' type='checkbox' id='" + actor.id + "'/>";
+		$("#checkbox").append(check);
+		check = "<label class='form-check-label' for='" + actor.id + "'>" + actor.name + "</label><br>";
+		$("#checkbox").append(check);
+
+
+	});
+};
 
 $(document).ready(() => {
 	$("button").click(() => {
 		// $("#test").hide();
+		// $.get(URL, dados, callback);
 		$.get("qualquer", (data, status) => {
 			alert("texto: " + data.texto + "\nStatus: " + data.statusCod);
 		});
